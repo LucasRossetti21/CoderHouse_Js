e07ecf247dde5d4ac289b4237473bc389399b751
Kosmo <95587250+LucasRossetti21@users.noreply.github.com>
2022-08-22 23:57:48 -0300

    Add files via upload

--- Desafio 1/app.js
+++ Desafio 1/app.js
@@ -6,10 +6,16 @@ function enviar(){
     if(monto.value >= 10000){
         cartel.innerHTML = `<p>El monto de: ${monto.value} es valido</p>
         <p>Tasa de interes: 60%</p>
-        <p>El monto total seria: ${monto.value * 1.60}</p>`
+        <p class="verde">Monto total a pagar: ${monto.value * 1.60}</p>`
     }
 
     else{
-        cartel.innerHTML = "El monto es menor a 10000"
+        cartel.innerHTML = `<p class="rojo">El monto es menor a 10000</p>`
     }
 }
+
+function bienvenido(){
+    window.alert("Bienvenido al desafio N° 1 :D")
+}
+
+bienvenido()
